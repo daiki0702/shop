ee1170c37e5866d32e968dd8ce099d0ba9f0a107
daiki0702 <daiki.tanaka.0702@gmail.com>
2020-08-05 09:36:42 +0900

    first commit

---
+++ php/s.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> 練習 </title>
+</head>
+<body>
+<?php
+$money = 30000;
+while( $money >= 0){
+	var_dump( $money );
+	$money = $money - 5080;
+}
+?>
+</body>
+</html>
---
+++ staff/staff_sample.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
+
+<?php
+	print 'こんにちは';
+	<br />
+	$hensuu = '何らかのデーター';
+	print $hensuu
+?>
+</body>
+</html>
